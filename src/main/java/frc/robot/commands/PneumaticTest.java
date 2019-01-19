a9ec8455550d9f7fca860c8cbdc7459a49073a0c
Josh Dutta <fainterpenny1234@gmail.com>
2019-01-19 15:44:18 -0500

    Adding the ability to retract the piston to the pneumatic test

--- src/main/java/frc/robot/commands/PneumaticTest.java
+++ src/main/java/frc/robot/commands/PneumaticTest.java
@@ -13,11 +13,13 @@ import frc.robot.Robot;
 
 public class PneumaticTest extends Command {
   DoubleSolenoid testSolenoid;
+  Boolean position;
   public PneumaticTest() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
     requires(Robot.PneumaticsTest);
     testSolenoid = new DoubleSolenoid(1,2); // 1 & 2 are port numbers, not sure which ports they are going in so change these as needed
+    position = false; //false is the default position where the piston is not extended and true is extended
   }
 
   // Called just before this Command runs the first time
@@ -29,13 +31,20 @@ public class PneumaticTest extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    if(position == false){
     testSolenoid.set(DoubleSolenoid.Value.kForward);
+    position = true;
   }
+    else{
+      testSolenoid.set(DoubleSolenoid.Value.kReverse);
+      position =false;
+    }
+}
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-   /* if(joystick button is pressed again){
+   /* if(joystick button is pressed(not the one used to execute)){
       return true;
    }*/
     return false;
