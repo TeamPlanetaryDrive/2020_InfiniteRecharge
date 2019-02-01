0834901dc58ec64097e7344cb5b5dcee3dea0d82
Josh Dutta <fainterpenny1234@gmail.com>
2019-01-31 20:54:52 -0500

    added methods to lift

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -6,8 +6,9 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
-
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
   //use to move the grippers up and down on the elevator
 /**
  * Add your docs here.
@@ -15,6 +16,26 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Lift extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  public byte target;
+  public double holeDistance;
+  public SpeedController motor;
+	
+	public void liftUp(double speed){
+		motor.set(speed);
+	} 
+		
+	public void liftDown(double speed){
+		motor.set(-speed);
+	}
+	
+	public void liftStop(){
+		motor.stopMotor();
+  }
+  public double Distance( double speed){
+    holeDistance =  speed *RobotMap.PERIODIC_UPDATE_PERIOD;
+    return holeDistance;
+  }
+
 
   @Override
   public void initDefaultCommand() {
