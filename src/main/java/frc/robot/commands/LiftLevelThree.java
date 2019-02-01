246d7183d50dd093551427d5a01a138df376760b
alanyluo <alan.luo@stu.fayette.kyschools.us>
2019-02-01 18:23:56 -0500

    added more functionality to lift levels

--- src/main/java/frc/robot/commands/LiftLevelThree.java
+++ src/main/java/frc/robot/commands/LiftLevelThree.java
@@ -27,6 +27,7 @@ public class LiftLevelThree extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.PIDMotor.setSetpoint(47);
   }
 
   // Make this return true when this Command no longer needs to run execute()
