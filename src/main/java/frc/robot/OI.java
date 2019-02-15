7fb5af396176c82cdd48cff57e1ec44ac1a95c0d
Josh Dutta <fainterpenny1234@gmail.com>
2019-02-14 21:28:41 -0500

    deleted all unneeded commands and cleaned up OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -19,37 +19,29 @@ import frc.robot.commands.grip.*;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
-  // initializing the joystick mappings
   public static int 
 		LEFT_JOYSTICK_PORT = 0,
     RIGHT_JOYSTICK_PORT = 1;
-  
-  public static Joystick leftJoystick = new Joystick(LEFT_JOYSTICK_PORT);
-	public static JoystickButton button2_left = new JoystickButton(leftJoystick, 2);
-	public static JoystickButton button3_left = new JoystickButton(leftJoystick, 3);
-	public static JoystickButton button4_left = new JoystickButton(leftJoystick, 4);
-	public static JoystickButton button5_left = new JoystickButton(leftJoystick, 5);
+   
+   public OI(){
+
+    final Joystick leftJoystick = new Joystick(LEFT_JOYSTICK_PORT);
+    final JoystickButton button2_left = new JoystickButton(leftJoystick, 2);
+    final JoystickButton button3_left = new JoystickButton(leftJoystick, 3);
+    final JoystickButton button4_left = new JoystickButton(leftJoystick, 4);
+    final JoystickButton button5_left = new JoystickButton(leftJoystick, 5);
 		
-	public static Joystick rightJoystick = new Joystick(RIGHT_JOYSTICK_PORT);
-	public static JoystickButton button2_right = new JoystickButton(rightJoystick, 2);
-	public static JoystickButton button3_right = new JoystickButton(rightJoystick, 3);
-	public static JoystickButton button4_right = new JoystickButton(rightJoystick, 4);
-  public static JoystickButton button5_right = new JoystickButton(rightJoystick, 5);
+    final Joystick rightJoystick = new Joystick(RIGHT_JOYSTICK_PORT);
+    final JoystickButton button2_right = new JoystickButton(rightJoystick, 2);
+    final JoystickButton button3_right = new JoystickButton(rightJoystick, 3);
+    final JoystickButton button4_right = new JoystickButton(rightJoystick, 4);
+    final JoystickButton button5_right = new JoystickButton(rightJoystick, 5);
 
   //This is where you put associate commands with all the buttons
-  public OI(){
     //Gripper controls
     button2_left.whenPressed(new ExtendGripPiston());
-    if(leftJoystick.getTrigger()){
+    if(leftJoystick.getTrigger())
       new RetractGripPiston();
-    }
 
     //Lift controls
     button4_left.whenPressed(new LiftLevelOne());
@@ -61,32 +53,7 @@ public class OI {
 
     //testing PID
     button2_right.whenPressed(new TestPID());
-  }
-
-  // left and right joysticks being mapped to ports and having their keys mapped (last years code)
-   public static void init(){
-
-   }
-
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
-
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
-
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
-
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
-
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
-
+    
+    }
   }
 
