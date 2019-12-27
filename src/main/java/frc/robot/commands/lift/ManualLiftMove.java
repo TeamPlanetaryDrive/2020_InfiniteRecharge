da800d02d2e7b9f5214b5da2453ba89ae89fc4fc
alanyluo <alan.luo@stu.fayette.kyschools.us>
2019-12-27 15:22:30 -0500

    removed HEAD files

--- src/main/java/frc/robot/commands/lift/ManualLiftMove.java
+++ src/main/java/frc/robot/commands/lift/ManualLiftMove.java
@@ -29,7 +29,7 @@ public class ManualLiftMove extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    System.out.println(RobotMap.liftEncoder.getDistance() + "Encoder");
+    // System.out.println(RobotMap.liftEncoder.getDistance() + "Encoder");
     if (RobotMap.leftJoystick.getTrigger()) {
       Robot.Elevator.enable();
       System.out.println("leftTrigger reached");
